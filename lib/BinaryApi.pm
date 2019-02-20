ff99c17da20946f2204ad250fb8c1394d68fe595
Aref Razavi <arefrazavi@gmail.com>
2019-02-20 22:28:46 +0330

    add get asset index

--- lib/BinaryApi.pm
+++ lib/BinaryApi.pm
@@ -5,7 +5,7 @@ use Mojo::UserAgent;
 use Data::Dumper;
 use feature 'say';
 
-sub get_stream {
+sub _show_stream {
     my ($underlying_symbol) = @_;
     my $ua = Mojo::UserAgent->new;
 
@@ -25,7 +25,69 @@ sub get_stream {
             ticks => $underlying_symbol
         } });
     });
+
     Mojo::IOLoop->start unless Mojo::IOLoop->is_running;
+}
+
+
+sub _get_symbols {
+    my ($active_symbols) = @_;
+    my $ua = Mojo::UserAgent->new;
+    my $symbols;
+
+    $ua->websocket('wss://ws.binaryws.com/websockets/v3?app_id=16047' => sub {
+        my ($ua, $tx) = @_;
+        unless ($tx->is_websocket) {
+            say 'WebSocket handshake failed!';
+            return;
+        }
+
+        $tx->on(json => sub {
+            my ($tx, $result) = @_;
+            $symbols = $result;
+        });
+
+        $tx->send({ json => {
+            active_symbols => $active_symbols || 'full'
+        } });
+
+    });
+
+    Mojo::IOLoop->start unless Mojo::IOLoop->is_running;
+    Mojo::IOLoop->stop;
+
+    return $symbols;
+
+}
+
+sub _get_assets {
+    my ($landing_company) = @_;
+    my $ua = Mojo::UserAgent->new;
+    my $assets;
+
+    $ua->websocket('wss://ws.binaryws.com/websockets/v3?app_id=16047' => sub {
+        my ($ua, $tx) = @_;
+        unless ($tx->is_websocket) {
+            say 'WebSocket handshake failed!';
+            return;
+        }
+
+        $tx->on(json => sub {
+            my ($tx, $result) = @_;
+            $assets = $result;
+        });
+
+        my $request = ($landing_company) ? {asset_index => 1, landing_company => $landing_company} : {asset_index => 1};
+
+        $tx->send({ json => $request });
+
+    });
+
+
+    Mojo::IOLoop->start unless Mojo::IOLoop->is_running;
+
+    return $assets;
+
 }
 
 1;
--- streams.pl
+++ streams.pl
@@ -3,7 +3,28 @@ use strict;
 use warnings FATAL => 'all';
 use BinaryApi;
 use Data::Dumper;
+use feature 'say';
 
 my $underlying_symbol = "R_100";
-BinaryApi::get_stream($underlying_symbol);
+#BinaryApi::_get_stream($underlying_symbol);
 
+#BinaryApi::_get_symbols());
+
+my $landing_company = 'costarica';
+my $assets = BinaryApi::_get_assets($landing_company);
+
+my $asset_keys = keys %$assets;
+
+say ref($assets->{asset_index});
+
+say "*** List of underlyings (instruments) and their available contract types for  $landing_company landing company ***";
+for my $asset_index (@{$assets->{asset_index}}) {
+    say "   Underlying Name: " . $asset_index->[0];
+    say "   Underlying Symbol: " . $asset_index->[1];
+    for my $contract_type (@{$asset_index->[2]}) {
+        say "   Contract Type: " . $contract_type->[0];
+        say "   Contract Name: " . $contract_type->[1];
+        say "   Min Contract Duration: " . $contract_type->[2];
+        say "   Max Contract Duration: " . $contract_type->[3] . "\n";
+    }
+}
